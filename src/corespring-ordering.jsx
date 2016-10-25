3e028f6af20f98c26f6a1dde8a9bf0f760612931
edeustace <ed.eustace@gmail.com>
2016-10-25 16:42:52 +0100

    with styles

--- src/corespring-ordering.jsx
+++ src/corespring-ordering.jsx
@@ -39,52 +39,55 @@ class CorespringOrdering extends React.Component {
     this.setState({showingCorrect: val});
   }
 
-
+  //TODO: can we set the drag cursor? https://github.com/RubaXa/Sortable/issues/246
   render() {
 
     const order = this.state.showingCorrect ? this.props.model.correctResponse : this.state.order;
+    
     const choices = order.map(
       (val, key) => {
         let choice = _.find(this.props.model.choices, (c) => { return c.id === val });
         let outcome = this.state.showingCorrect ? {outcome: 'correct'} : _.find(this.props.model.outcomes, (c) => { return c.id === val });
-        let choiceClass = 'choice ' + (outcome || {}).outcome;
+        let choiceClass = 'choice ';
+        choiceClass += (outcome || {outcome: ''}).outcome;
+        choiceClass += this.props.model.disabled ? ' disabled' : '';
+
         return <div className="choice-list-item" key={key} data-id={val}><div className={choiceClass}>{choice.label}</div></div>;
       }
     );
 
     const toggler = this.props.model.correctResponse ? <CorespringShowCorrectAnswerToggle initialValue={false} onToggle={this.toggleCorrect.bind(this)}/> : null;
-    const className =  "corespring-ordering " //+ this.props.model.env.mode;
+    let className =  "corespring-ordering " //+ this.props.model.env.mode;
+    className += this.props.model.className || '';
+    
+    let sortOpts = {
+       animation: 150,
+       disabled: this.props.model.disabled
+     };
+        
+    let sortChange = (order, sortable, evt) => {
+      this.setState({order: order});
+      this.props.session.value = order;
+    };
+
     let myAnswer = (className) => { return <div className={className} key={1}>
       <Sortable
-        options={{
-          animation: 150,
-          disabled: this.props.model.disabled
-        }}
+        options={sortOpts}
         className="choice-list"
         tag="div"
-        onChange={(order, sortable, evt) => {
-          this.setState({order: order});
-          this.props.session.value = order;
-        }}
-      >
+        onChange={sortChange}>
         {choices}
       </Sortable>
     </div>; }
+    
     const maybeMyAnswer = this.state.showingCorrect ? '' : myAnswer('choices-wrapper');
 
     const correctAnswer = this.state.showingCorrect ? <div className="choices-wrapper" key={2}>
       <Sortable
-        options={{
-          animation: 150,
-          disabled: this.props.model.disabled
-        }}
+        options={sortOpts} 
         className="choice-list"
         tag="div"
-        onChange={(order, sortable, evt) => {
-          this.setState({order: order});
-          this.props.session.value = order;
-        }}
-      >
+        onChange={sortChange}>
         {choices}
       </Sortable>
     </div> : '';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './tmp.ordering.jsx';
+import Main from './corespring-ordering.jsx';
 
 var style = require('!style!css!less!./index.less');
 
